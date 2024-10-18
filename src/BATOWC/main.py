744d4d69c3648ff7bf358ee3e950b6ce233c2cfc
Remote System <60648387+alwism@users.noreply.github.com>
2024-10-18 20:03:41 +0200

    Added wxpython

--- src/BATOWC/main.py
+++ src/BATOWC/main.py
@@ -1,8 +1,7 @@
 from .logger import log
 def main():
     log("Try printing Hello world")
-    print_hi('World')
-    log("Hello world printed")
+    return print_hi('World')
 
 def print_hi(name):
-    print(f'Hello, {name}')
+    return f'Hello, {name}'
